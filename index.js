a819c05400b479ab830f520e6a894a338e1276fe
Jory Roberts <robertsjory13@gmail.com>
2023-04-05 06:07:57 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const element = document.querySelector("#text");
-  element.textContent = "This is so cool!";
-});
+  let p = document.querySelector('#text');
+  p.textContent = "This is really cool!";
+  
+})
+
+let p = document.querySelector('p');
+p.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
